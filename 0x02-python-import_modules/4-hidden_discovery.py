2cfd2d31c32e095cd52416387141174a8e944bd7
Afiya21 <88346482+Afiya21@users.noreply.github.com>
2021-11-18 22:40:55 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
 import hidden_4
 
-l = dir(hidden_4)
+
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
 
 if __name__ == "__main__":
-    for i in range(0, len(l)):
-        if l[i][0] != "_" and l[i][1] != "_":
-            print(l[i])
+    discovr()
