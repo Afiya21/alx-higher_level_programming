f8e4e490108d6cf6912d626e5e573095a8e080b6
Afiya21 <88346482+Afiya21@users.noreply.github.com>
2021-11-24 19:20:46 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+def best_score(my_dict):
+    if my_dict is None or my_dict == {}:
         return None
-    return max(a_dictionary.keys())
+    biggest = max(my_dict.values())
+    for key, value in my_dict.items():
+        if value is biggest:
+            return key
