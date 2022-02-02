7ec48e887598a6d544805a81fd59a24241b50783
Afiya21 <88346482+Afiya21@users.noreply.github.com>
2022-02-02 14:27:52 +0300

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
--- 2. Read user
--- a script that creates the database hbtn_0d_2 and the user user_0d_2
+-- Create a datbase and and user with Select only privlage
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
