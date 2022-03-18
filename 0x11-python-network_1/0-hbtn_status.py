86214c7024dbd813f8dc414ce5bf95bb0fc97f17
Afiya21 <NebyatAhmed21@gmail.com>
2022-03-18 16:20:17 +0300

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,14 +2,17 @@
 """
 fetch alx-intranet.hbtn.io/status
 """
+import urllib.request
 
-if __name__ == '__main__':
-    import urllib.request
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
+
+def fetcher():
+    """fetcher"""
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
 
-    print("Body response:")
-    print("\t- type: {}".format(html.__class__))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('ascii')))
+if __name__ == "__main__":
+    fetcher()
