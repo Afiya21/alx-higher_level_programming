44ed1c00205d395630f8ace98bacffce2b052e1a
Afiya21 <88346482+Afiya21@users.noreply.github.com>
2022-04-12 19:36:36 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
 const url = process.argv[2];
-request.get(url, function (err, response, body) {
-  let count = 0;
-  if (err) {
-    console.log(err);
-  }
-  const data = JSON.parse(body);
-  for (let i = 0; data.results[i] !== undefined; i++) {
-    if (data.results[i].characters.includes('https://swapi-api.hbtn.io/api/films/18/')) {
-      count++;
+request(url, (err, res, body) => {
+  if (err) { console.log(err); } else if (res.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const idx in films) {
+      const filmChars = films[idx].characters;
+      for (const cidx in filmChars) {
+        if (filmChars[cidx].includes('18')) { count++; }
+      }
     }
-  }
-  console.log(count);
+    console.log(count);
+  } else { console.log('An error occured. Status code: ' + res.statusCode); }
 });
